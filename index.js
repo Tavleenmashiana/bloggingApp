4a43fb818b581fc360469a7e939f173d9f0028b3
Tavleen <tavleenmashiana@gmail.com>
2022-08-11 15:46:48 +0530

    second-commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const xhr = new XMLHttpRequest();
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+xhr.open('GET', url)
+
+xhr.onreadystatechange =() => {
+    if(xhr.status === 200 && xhr.readyState === 4) {
+        const response = JSON.parse(xhr.responseText)
+        console.log(response)
+
+     
+    let template = '';
+    for(let i=0; i<response.length; i++) {
+         template +=`        
+         <div class="content">
+            <h2 class="card-header">${response[i].title}</h2>
+            <div class="card-body">
+               
+                <p class="card-text">${response[i].body}</p>
+            </div>
+        </div>`
+    }
+    document.querySelector('#mycontainer').innerHTML = template;
+}
+};
+xhr.send()
